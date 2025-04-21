e5bb543b13c61a29e1ccbbb8c8bb07c5755ac8de
Nasrulhaq Hidayat Abdul Latiff <nasrulhaqhidayat@gmail.com>
2025-04-21 11:50:49 +0800

    add csrf token, jquery, and fontawesome for icon also @stack for push any scripts

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,11 +1,14 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <title>{{ $title ?? 'Laravel' }}</title>
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 <!-- Include Flowbite JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://kit.fontawesome.com/d46a78d0a5.js" crossorigin="anonymous"></script>
+@stack('scripts')
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
