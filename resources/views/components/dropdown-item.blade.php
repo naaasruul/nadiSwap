92bf1786045fc7b075a1f6cef08b28736c9a30f0
Nasrulhaq Hidayat Abdul Latiff <nasrulhaqhidayat@gmail.com>
2025-04-21 18:36:45 +0800

    component dropdown item

---
+++ resources/views/components/dropdown-item.blade.php
@@ -0,0 +1,24 @@
+@props(['label', 'icon', 'items' => []])
+
+<li>
+    <button type="button" 
+        class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" 
+        aria-controls="dropdown-{{ Str::slug($label) }}" 
+        data-collapse-toggle="dropdown-{{ Str::slug($label) }}">
+        <i class="{{ $icon }} shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"></i>
+        <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">{{ $label }}</span>
+        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+        </svg>
+    </button>
+    <ul id="dropdown-{{ Str::slug($label) }}" class="hidden py-2 space-y-2">
+        @foreach ($items as $item)
+            <li>
+                <a href="{{ $item['route'] ?? '#' }}" 
+                   class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                    {{ $item['label'] }}
+                </a>
+            </li>
+        @endforeach
+    </ul>
+</li>
