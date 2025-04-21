66f4388a25d239eda5639fad991fa159e6bb5619
Nasrulhaq Hidayat Abdul Latiff <nasrulhaqhidayat@gmail.com>
2025-04-21 23:06:51 +0800

    tambah relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -68,4 +68,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Review::class);
     }
+
+    public function deliveryAddresses()
+    {
+        return $this->hasMany(DeliveryAddress::class);
+    }
 }
