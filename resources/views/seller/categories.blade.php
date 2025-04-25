c4ce95b62c8a105b05e24b5f249b80a400ac5689
Nasrulhaq Hidayat Abdul Latiff <nasrulhaqhidayat@gmail.com>
2025-04-26 01:05:39 +0800

    add CategoryController, Category model, migration, and categories view

---
+++ resources/views/seller/categories.blade.php
@@ -0,0 +1,33 @@
+<x-layouts.app :title="__('Categories')">
+    <x-dashboard-header>Categories</x-dashboard-header>
+    
+    @if (session('success'))
+    <div class="p-4 my-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
+        role="alert">
+        {{ session('success') }}
+    </div>
+    @endif
+
+    @if ($errors->any())
+    <div class="p-4 my-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+
+    <!-- Add Product Modal Toggle -->
+    <div class="w-full flex justify-end mb-4">
+        <button data-modal-target="create-category" data-modal-toggle="create-category"
+            class="block text-white bg-pink-700 hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800"
+            type="button">
+            Add Category
+        </button>
+    </div>
+
+    
+
+
+</x-layouts.app>
