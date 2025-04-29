36f2d10f586ecbf4dd81e1d35f4c04c53b0af338
Muhammad Zaim <linkguardian878@gmail.com>
2025-04-29 21:42:31 +0800

    use migration created by artisan instead of relying on created ones so it's actually rollback-able

--- database/migrations/2025_04_29_083302_add_weight_to_user_category_preference.php
+++ database/migrations/2025_04_29_083302_add_weight_to_user_category_preference.php
@@ -4,19 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddWeightToUserCategoryPreferencesTable extends Migration
+return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up()
     {
         Schema::table('user_category_preferences', function (Blueprint $table) {
-            $table->integer('weight')->default(1);
+            $table->decimal('weight', 10, 2)->default(0);
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down()
     {
         Schema::table('user_category_preferences', function (Blueprint $table) {
             $table->dropColumn('weight');
         });
     }
-}
+};
