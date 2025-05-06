ece11dc3af541f8d9a234639212269230618ed4c
Nasrulhaq Hidayat Abdul Latiff <nasrulhaqhidayat@gmail.com>
2025-05-07 02:46:56 +0800

    add file_receipt column to orders table migration

---
+++ database/migrations/2025_05_06_173536_add_file_receipt_to_orders_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('file_receipt')->nullable()->after('items'); // Add the file_receipt column
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('file_receipt'); // Remove the file_receipt column
+        });
+    }
+};
