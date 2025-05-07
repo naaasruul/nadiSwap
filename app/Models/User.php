2bfb5c5cd308c6de0698c5c5b6ece869cec25432
Nasrulhaq Hidayat Abdul Latiff <nasrulhaqhidayat@gmail.com>
2025-05-08 01:11:07 +0800

    add bankAccount relationship to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -85,4 +85,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(UserCategoryPreference::class);
     }
+
+    public function bankAccount()
+    {
+        return $this->hasOne(BankAccount::class);
+    }
 }
