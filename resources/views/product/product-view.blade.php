8e00b2367ed624cab35d94103bb356653d23d678
Nasrulhaq Hidayat Abdul Latiff <nasrulhaqhidayat@gmail.com>
2025-05-24 11:01:40 +0800

    client taknak tunjuk review untk seller sebba dah ada page seller.

--- resources/views/product/product-view.blade.php
+++ resources/views/product/product-view.blade.php
@@ -43,25 +43,26 @@
                         <p class="text-2xl font-extrabold text-gray-900 sm:text-3xl dark:text-white">
                             RM{{ $product->price }}
                         </p>
-                        <div class="flex items-center gap-2 mt-2 sm:mt-0">
-                            <div class="flex items-center gap-1">
-                                @for ($i = 1; $i <= 5; $i++) <svg
-                                    class="w-4 h-4 {{ $i <= $averageRating ? 'text-yellow-300' : 'text-gray-300 dark:text-gray-500' }}"
-                                    aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                    viewBox="0 0 24 24">
-                                    <path
-                                        d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z" />
-                                    </svg>
-                                @endfor
+                        @hasrole('buyer')
+                            <div class="flex items-center gap-2 mt-2 sm:mt-0">
+                                <div class="flex items-center gap-1">
+                                    @for ($i = 1; $i <= 5; $i++) <svg
+                                        class="w-4 h-4 {{ $i <= $averageRating ? 'text-yellow-300' : 'text-gray-300 dark:text-gray-500' }}"
+                                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                        <path
+                                            d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z" />
+                                        </svg>
+                                        @endfor
+                                </div>
+                                <p class="text-sm font-medium leading-none text-gray-500 dark:text-gray-400">
+                                    ({{ number_format($averageRating, 1)}})
+                                </p>
+                                <a href="#review-section"
+                                    class="text-sm font-medium leading-none text-gray-900 underline hover:no-underline dark:text-white">
+                                    {{ $product->reviews_count }} Reviews
+                                </a>
                             </div>
-                            <p class="text-sm font-medium leading-none text-gray-500 dark:text-gray-400">
-                                ({{ number_format($averageRating, 1)}})
-                            </p>
-                            <a href="#review-section"
-                                class="text-sm font-medium leading-none text-gray-900 underline hover:no-underline dark:text-white">
-                                {{ $product->reviews_count }} Reviews
-                            </a>
-                        </div>
+                        @endrole           
                     </div>
 
                     <!-- Add to Cart Form -->
@@ -122,6 +123,7 @@
     </x-section>
 
     {{-- review section  --}}
+    @hasrole('buyer')
     <x-section  class="bg-white py-8 antialiased dark:bg-gray-900 md:py-16">
         <div id='review-section' class="mx-auto max-w-screen-xl px-4 2xl:px-0">
             <h2 class="text-2xl font-semibold text-gray-900 dark:text-white">Reviews</h2>
@@ -148,6 +150,7 @@
             </div>
         </div>
     </x-section>
+    @endrole
 
 
     <!-- Add review modal -->
