1c338fdfacf46befacb5025d0f69f27281a66c05
Muhammad Zaim <linkguardian878@gmail.com>
2025-06-05 01:22:49 +0800

    feat: added fields

--- resources/views/components/order-list.blade.php
+++ resources/views/components/order-list.blade.php
@@ -1,4 +1,4 @@
-@props(['orderId', 'date', 'price', 'delivery_status', 'payment_status', 'payment_method', 'actions' => []])
+@props(['orderId', 'date', 'price', 'delivery_status', 'payment_status', 'payment_method', 'order_status', 'actions' => []])
 
 <div class="flex flex-wrap items-center gap-y-4 border-b border-gray-200 pb-4 dark:border-gray-700 md:pb-5">
     <dl class="w-1/2 sm:w-48">
@@ -15,7 +15,7 @@
 
     <dl class="w-1/2 sm:w-1/5 md:flex-1 lg:w-auto">
         <dt class="text-base font-medium text-gray-500 dark:text-gray-400">Price:</dt>
-        <dd class="mt-1.5 text-base font-semibold text-gray-900 dark:text-white">{{ (number_format($price, 2)) }}</dd>
+        <dd class="mt-1.5 text-base font-semibold text-gray-900 dark:text-white">RM {{ (number_format($price, 2)) }}</dd>
     </dl>
 
     <dl class="w-1/2 sm:w-1/5 md:flex-1 lg:w-auto">
@@ -23,10 +23,19 @@
         <dd class="mt-1.5 text-base font-semibold text-gray-900 dark:text-white">{{ ($payment_method == 'online_banking' ? 'Online Banking' : 'Cash On Delivery') }}</dd>
     </dl>
 
+    @php
+        $deliveryClasses = match ($delivery_status) {
+            'shipped' => 'bg-blue-100 text-blue-800',
+            'ofd' => 'bg-purple-100 text-purple-800',
+            'delivered' => 'bg-green-100 text-green-800',
+            'cancelled' => 'bg-red-100 text-red-800',
+            default => 'bg-yellow-100 text-yellow-800',
+        };
+    @endphp
     <dl class="w-1/2 sm:w-1/4 sm:flex-1 lg:w-auto">
         <dt class="text-base font-medium text-gray-500 dark:text-gray-400">Delivery Status:</dt>
         <dd class="me-2 mt-1.5 inline-flex shrink-0 items-center rounded capitalize
-        {{ $delivery_status == 'delivered' ? 'bg-green-100 text-green-800' : ($delivery_status == 'pending' ? 'bg-yellow-100 text-yellow-800' : 'bg-red-100 text-red-800') }}
+        {{ $deliveryClasses }}
          px-2.5 py-0.5 text-xs font-medium">
             <svg class="me-1 h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                 fill="none" viewBox="0 0 24 24">
@@ -34,14 +43,23 @@
                     d="M13 7h6l2 4m-8-4v8m0-8V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v9h2m8 0H9m4 0h2m4 0h2v-4m0 0h-5m3.5 5.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Zm-10 0a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z">
                 </path>
             </svg>
-            {{ $delivery_status }}
+            {{ $delivery_status == 'ofd' ? 'Out for Delivery' : ucfirst($delivery_status) }}
         </dd>
     </dl>
 
+    @php
+        $paymentClasses = match ($payment_status) {
+            'pending' => 'bg-yellow-100 text-yellow-800',
+            'paid' => 'bg-green-100 text-green-800',
+            'failed' => 'bg-red-100 text-red-800',
+            'cancelled' => 'bg-red-100 text-red-800',
+            default => 'bg-gray-100 text-gray-800',
+        };
+    @endphp
     <dl class="w-1/2 sm:w-1/4 sm:flex-1 lg:w-auto">
         <dt class="text-base font-medium text-gray-500 dark:text-gray-400">Payment Status:</dt>
         <dd class="me-2 mt-1.5 inline-flex shrink-0 items-center rounded  capitalize
-        {{ $payment_status == 'completed' ? 'bg-green-100 text-green-800' : ($payment_status == 'pending' ? 'bg-yellow-100 text-yellow-800' : 'bg-red-100 text-red-800') }}
+        {{ $paymentClasses }}
          px-2.5 py-0.5 text-xs font-medium">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="me-1 h-3 w-3"
                 viewBox="0 0 16 16">
@@ -52,7 +70,29 @@
         </dd>
     </dl>
 
-    {{-- <div class="w-full sm:flex sm:w-32 sm:items-center sm:justify-end sm:gap-4">
+    @php
+        $orderClasses = match ($order_status) {
+            'pending' => 'bg-yellow-100 text-yellow-800',
+            'completed' => 'bg-green-100 text-green-800',
+            'cancelled' => 'bg-red-100 text-red-800',
+            default => 'bg-gray-100 text-gray-800',
+        };
+    @endphp
+    <dl class="w-1/2 sm:w-1/4 sm:flex-1 lg:w-auto">
+        <dt class="text-base font-medium text-gray-500 dark:text-gray-400">Order Status:</dt>
+        <dd class="me-2 mt-1.5 inline-flex shrink-0 items-center rounded  capitalize
+        {{ $orderClasses }}
+         px-2.5 py-0.5 text-xs font-medium">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="me-1 h-3 w-3"
+                viewBox="0 0 16 16">
+                <path
+                    d="M0 3a2 2 0 0 1 2-2h13.5a.5.5 0 0 1 0 1H15v2a1 1 0 0 1 1 1v8.5a1.5 1.5 0 0 1-1.5 1.5h-12A2.5 2.5 0 0 1 0 12.5zm1 1.732V12.5A1.5 1.5 0 0 0 2.5 14h12a.5.5 0 0 0 .5-.5V5H2a2 2 0 0 1-1-.268M1 3a1 1 0 0 0 1 1h12V2H2a1 1 0 0 0-1 1" />
+            </svg>
+            {{ $order_status }}
+        </dd>
+    </dl>
+
+    <div class="w-full sm:flex sm:w-32 sm:items-center sm:justify-end sm:gap-4">
         <button id="actionsMenuDropdownModal{{ $orderId }}" data-dropdown-toggle="dropdownOrderModal{{ $orderId }}"
             type="button"
             class="flex w-full items-center justify-center rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-pink-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700 md:w-auto">
@@ -79,5 +119,5 @@
                 @endforeach
             </ul>
         </div>
-    </div> --}}
+    </div> 
 </div>
