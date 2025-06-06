f9bc24bcc50660dfaaf1a02bd2998c855a6b9c67
Muhammad Zaim <linkguardian878@gmail.com>
2025-06-06 18:17:11 +0800

    feat: qol

--- public/js/admin-payment-table.js
+++ public/js/admin-payment-table.js
@@ -29,10 +29,17 @@ if (document.getElementById("admin-order-table") && typeof simpleDatatables.Data
                 <option value="failed">Failed</option>
             </select>` + 
 			(options.searchable ?
-				`<div class=" + ${options.classes.search} + ">` +
-					"<input class='" + options.classes.input + "' placeholder='" + options.labels.placeholder + "' type='search' title='" + options.labels.searchTitle + "'" + (dom.id ? " aria-controls='" + dom.id + "'" : "") + ">" +
-				"</div>" : ""
-			) +
+                `<div class=" + ${options.classes.search} + ">` +
+                    `<div class="relative">` +
+                        `<div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                            </svg>
+                        </div>` +
+                        "<input class='block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 " + options.classes.input +  "' placeholder='" + options.labels.placeholder + "' type='search' title='" + options.labels.searchTitle + "'" + (dom.id ? " aria-controls='" + dom.id + "'" : "") + ">" +
+                    "</div>" +
+                "</div>" : ""
+            ) +
 		"</div>" +
 		"<div class='" + options.classes.container + "'" + (options.scrollY.length ? " style='height: " + options.scrollY + "; overflow-Y: auto;'" : "") + "></div>" +
 		"<div class='" + options.classes.bottom + "'>" +
